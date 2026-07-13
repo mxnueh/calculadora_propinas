6b998b53d60f703e37d3b3d2fb205c4026a4ab0d
enmanuel <enmanuelnunezmatias@gmail.com>
2026-07-13 15:00:10 -0400

    feat: initialize project with React, TypeScript, and Tailwind CSS
    - Add package.json with dependencies and scripts for development
    - Create favicon and icons SVG files for branding
    - Implement main application structure in App.tsx
    - Define menu items data structure in db.ts
    - Set up global styles with Tailwind CSS in index.css
    - Configure TypeScript with appropriate settings in tsconfig files
    - Set up Vite configuration for React and Tailwind CSS integration

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { menuItems } from "./data/db";
+
+function App() {
+  console.log(menuItems);
+  return (
+    <>
+      <header className="bg-teal-400 py-5">
+        <h1 className="text-center text-4xl font-black">Calculadora de Propinas y Consumo</h1>
+      </header>
+    </>
+  )
+}
+
+export default App
