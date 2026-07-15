f3152ff501f14fc001d34c66edc7b5760a7aab15
enmanuel <enmanuelnunezmatias@gmail.com>
2026-07-15 14:36:58 -0400

    refactor: improve Menu and MenuItem component structure and styling

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -6,9 +6,9 @@ type MenuItemProps = {
 
 export default function MenuItem({item}: MenuItemProps) {
   return (
-    <>
+    <button className="border-2 border-teal-400 hover:bg-teal-200 w-full p-3 flex justify-between border rounded-md">
       <p>{item.name}</p>
       <p className="font-black">${item.price}</p>
-    </>
+    </button>
   )
 }
