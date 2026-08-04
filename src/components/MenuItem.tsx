fa68223c8259ba00f79e47b0eaefe355e6b92f90
enmanuel <enmanuelnunezmatias@gmail.com>
2026-08-03 23:41:56 -0400

    fix: correct button border class in MenuItem component and add useOrder hook

--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -6,7 +6,7 @@ type MenuItemProps = {
 
 export default function MenuItem({item}: MenuItemProps) {
   return (
-    <button className="border-2 border-teal-400 hover:bg-teal-200 w-full p-3 flex justify-between border rounded-md">
+    <button className="border-teal-400 hover:bg-teal-200 w-full p-3 flex justify-between border-2 rounded-md">
       <p>{item.name}</p>
       <p className="font-black">${item.price}</p>
     </button>
---
+++ src/hooks/useOrder.ts
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+export default function useOrder() {
+    const [order, setOrder] = useState([]);
+
+    console.log(order);
+
+    return {
+
+    }
+}
