c2996f2f0e11980afd31a9937c0a9aee097539c8
enmanuel <enmanuelnunezmatias@gmail.com>
2026-08-05 22:00:38 -0400

    feat: define OrderItem type and update useOrder hook state management

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -1,7 +1,10 @@
 import { useState } from 'react';
+import type { OrderItem } from '../types'; 
 
 export default function useOrder() {
-    const [order, setOrder] = useState([]);
+    const [order, setOrder] = useState<OrderItem[]>([]);
+    const [total, setTotal] = useState<number>(0);
+    const [auth, setAuth] = useState<boolean>(false);
 
     console.log(order);
 
